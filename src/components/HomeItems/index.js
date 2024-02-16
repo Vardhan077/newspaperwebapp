4d10afcc224fc9c94472e34134510a66bc730648
Vardhan077 <vardhanmarriwada777@gmail.com>
2024-02-16 23:39:10 +0530

    another

--- src/components/HomeItems/index.js
+++ src/components/HomeItems/index.js
@@ -1,47 +1,3 @@
-// import React, { useState } from 'react';
-// import { Navigate,Link } from 'react-router-dom'
-// import ArticleDetailScreen from '../ArticleDetailScreen'
-// import './index.css'
-
-// const HomeItems = props =>{
-//     const [imageSrc, setImageSrc] = useState('original_image_url.jpg');
-//     const {item,id} = props
-//     console.log(id,'is url from home item')
-//     const {urlToImage,author,content,description,publishedAt,title} = item
-
-//     const handleImageError = () => {
-//         setImageSrc('https://res.cloudinary.com/dg9itycrz/image/upload/v1708069245/pexels-nappy-935979_x4rtsx.jpg');
-//       };
-
-//     return(
-//         <Link to={`/article/${id}`} className='item' >
-//             <div className="homeItem" >
-//                 <div className='itemIn'>
-//                     <img src={urlToImage} className="itemImg" alt = {'Image'} onError={handleImageError} />
-//                     <h2 className='itemTitle'>{title}</h2>
-//                     {/* <h3 className='itemContent'>{content}</h3> */}
-//                     {/* <p>{description}</p> */}
-//                     <div className='in'>
-//                         {/* <p className='itemAuthor'>{author}</p> */}
-//                         <br/>{"\n"}
-//                         <p className='itemPublished'>{publishedAt}</p>
-//                     </div>
-//                 </div>
-//             </div>
-//         </Link>
-        
-//     )
-    
-// }
-
-
-// export default HomeItems
-
-
-
-
-
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './index.css';
@@ -49,23 +5,16 @@ import './index.css';
 const HomeItems = (props) => {
   const { item, id } = props;
   const { urlToImage, author, content, description, publishedAt, title } = item;
-
-  // State to manage image source
   const [imageSrc, setImageSrc] = useState(urlToImage);
-  // State to track image loading status
+
   const [imageLoadError, setImageLoadError] = useState(false);
 
-  // Event handler for image error
   const handleImageError = () => {
-    // Set alternative image URL if original image fails to load
-    console.log('EROOOOOOOOOOOOOOOOOOOOOOOOOORRRRRRR')
     setImageSrc('https://res.cloudinary.com/dg9itycrz/image/upload/v1708069245/pexels-nappy-935979_x4rtsx.jpg');
     setImageLoadError(true);
   };
 
-  // Check if image failed to load
   if (imageLoadError) {
-    // Render an error message or placeholder image
     return (
       <div>
         <p>Failed to load image</p>
@@ -78,13 +27,9 @@ const HomeItems = (props) => {
     <Link to={`/article/${id}`} className='item'>
       <div className="homeItem">
         <div className='itemIn'>
-          {/* Use imageSrc state for the image source */}
           <img src={imageSrc} className="itemImg" alt='Image' onError={handleImageError} />
           <h2 className='itemTitle'>{title}</h2>
-          {/* <h3 className='itemContent'>{content}</h3> */}
-          {/* <p>{description}</p> */}
           <div className='in'>
-            {/* <p className='itemAuthor'>{author}</p> */}
             <br />
             {"\n"}
             <p className='itemPublished'>{publishedAt}</p>
--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -17,7 +17,6 @@ class HomeScreen extends Component{
 
     componentDidMount(){
         this.getData()
-        // this.getHeadlines()
     }
 
 
@@ -60,14 +59,14 @@ class HomeScreen extends Component{
                     <p className="sideTitle">{title}</p>
                     <p className="sideAuthor">{author}</p>
                 </Link>
-                {/* <p className="sideRead">2 MIN READ</p> */}
+           
             </li>
         )
     }
 
     youShouldKnowFunTwo = () =>{
         const {list} = this.state
-        // let a = list.slice(5,9)
+   
         return(list.map(item =>(
             <HomeItems item = {item} key = {list.indexOf(item)} id = {encodeURIComponent(item.url)} />
         )))
@@ -106,7 +105,7 @@ class HomeScreen extends Component{
         return(
             <div>
                 <div className="nav">
-                    <h2 className="head">Sign Up for Our Newsletter>></h2>
+                    <h2 className="head">Sign Up for Our Newsletter</h2>
                     <h1 className="heading">BHARAT NEWS</h1>
                     <div className="in">
                         <p className="signin">SIGN IN</p>
@@ -154,17 +153,12 @@ class HomeScreen extends Component{
                     
                 </div>
                 <hr className="line lineT"/>
-                {/* <div className="youShouldKnow">
-                    <h1 className="second">EDITOR'S PICKS</h1>
-                    <ul className="ysk">
-                        {this.editorsPicksFun()}
-                    </ul>
-                </div> */}
+            
 
                 <div className="videoPlayer">
                     <div>
                         <div className="full">
-                            <h1 className="video">VIDEO<span style = {{color:'red'}}>></span></h1>
+                            <h1 className="video">VIDEO<span style = {{color:'red'}}></span></h1>
                             <ReactPlayer className = 'player' controls = {true} pip = {false} url = 'https://www.youtube.com/watch?v=KOEfDvr4DcQ' height='360px' width='640px' />
                         </div>
                         <h2 className="headingVid">
@@ -191,7 +185,7 @@ class HomeScreen extends Component{
                 </div>
 
                 <div className="youShouldKnowTwo">
-                    <h1 className="second">MORE TO KNOW<span style={{color:'red'}}>></span></h1>
+                    <h1 className="second">MORE TO KNOW<span style={{color:'red'}}></span></h1>
                     <ul className="yskTwo">
                         {this.youShouldKnowFunTwo()}
                     </ul>
@@ -232,141 +226,3 @@ export default HomeScreen
 
 
 
-// import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-// import HomeItems from "../HomeItems";
-// import ReactPlayer from "react-player";
-// import { CiFacebook } from "react-icons/ci";
-// import { BsInstagram } from "react-icons/bs";
-// import { FiTwitter } from "react-icons/fi";
-// import "./index.css";
-
-// class HomeScreen extends Component {
-//   state = { list: [] };
-
-//   componentDidMount() {
-//     this.getData();
-//   }
-
-//   getData = async () => {
-//     try {
-//       const url =
-//         "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=690db48467224d73b39d478ed4cc83a2";
-//       const response = await fetch(url);
-//       if (!response.ok) {
-//         throw new Error("Failed to fetch data");
-//       }
-//       const data = await response.json();
-//       const filteredArticles = data.articles.filter((item) => item.urlToImage);
-//       const articles = filteredArticles.slice(2, 19);
-//       this.setState({ list: articles });
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//     }
-//   };
-
-//   render() {
-//     const { list } = this.state;
-//     const main = list.length > 0 ? list[0] : null;
-
-//     return (
-//       <div>
-//         <div className="nav">
-//           <h2 className="head">Sign Up for Our Newsletter>></h2>
-//           <h1 className="heading">BHARAT NEWS</h1>
-//           <div className="in">
-//             <p className="signin">SIGN IN</p>
-//             <button className="button">SUBSCRIBE</button>
-//           </div>
-//         </div>
-//         <hr className="line" />
-//         <div className="search">
-//           <input type="search" placeholder="Search" />
-//         </div>
-//         <div className="mainBox">
-//           {main && (
-//             <Link to="/main" className="mainMatter">
-//               <h1 className="mainTitle">{main.title}</h1>
-//               <p className="read">7 MIN READ</p>
-//               <p className="mainPublished">{main.publishedAt}</p>
-//               <p className="mainDesc">{main.description}</p>
-//             </Link>
-//           )}
-//           {main && (
-//             <Link to="/main">
-//               <img src={main.urlToImage} className="mainImg" alt="Main" />
-//             </Link>
-//           )}
-//           <div className="mmmm">
-//             <div className="mostRead">
-//               <h1 className="sidehead">MOST READ</h1>
-//               <ol className="ordered">
-//                 {list.slice(1, 9).map((item) => (
-//                   <li key={item.url}>
-//                     <Link to={`/article/${encodeURIComponent(item.url)}`} className="mostReadLink">
-//                       <p className="sideTitle">{item.title}</p>
-//                       <p className="sideAuthor">{item.author}</p>
-//                     </Link>
-//                   </li>
-//                 ))}
-//               </ol>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="newsItems">
-//           {list.slice(0, 3).map((item) => (
-//             <HomeItems key={item.url} item={item} id={encodeURIComponent(item.url)} />
-//           ))}
-//         </div>
-//         <hr className="line lineT" />
-//         <div className="youShouldKnow">
-//           <h1 className="second">YOU SHOULD KNOW</h1>
-//           <ul className="ysk">
-//             {list.slice(5, 9).map((item) => (
-//               <HomeItems key={item.url} item={item} id={encodeURIComponent(item.url)} />
-//             ))}
-//           </ul>
-//         </div>
-//         <hr className="line lineT" />
-//         <div className="videoPlayer">
-//           <div className="full">
-//             <h1 className="video">VIDEO<span style={{ color: "red" }}>></span></h1>
-//             <ReactPlayer className="player" controls={true} pip={false} url="https://www.youtube.com/watch?v=KOEfDvr4DcQ" height="360px" width="640px" />
-//           </div>
-//           <h2 className="headingVid">Face Your Biggest Fear To Win $800,000</h2>
-//           <div className="smallVid">
-//             <ReactPlayer className="playerS" controls={true} pip={false} url="https://www.youtube.com/watch?v=Q6G5eYNPuiA" height="150px" width="270px" />
-//             <h1 className="smallHeading">Indian bowlers with Most Test wickets | WION</h1>
-//           </div>
-//           <div className="smallVid">
-//             <ReactPlayer className="playerS" controls={true} pip={false} url="https://www.youtube.com/watch?v=7JXkKVA5RAA" height="150px" width="270px" />
-//             <h1 className="smallHeading">Will Paytm CRASH or Make a COMEBACK?</h1>
-//           </div>
-//           <div className="smallVid">
-//             <ReactPlayer className="playerS" controls={true} pip={false} url="https://www.youtube.com/watch?v=d7YXPHU8Dr4" height="150px" width="270px" />
-//             <h1 className="smallHeading">Behind The Scenes Of Shark Tank India</h1>
-//           </div>
-//         </div>
-//         <div className="youShouldKnowTwo">
-//           <h1 className="second">MORE TO KNOW<span style={{ color: "red" }}>></span></h1>
-//           <ul className="yskTwo">
-//             {list.slice(2, 6).map((item) => (
-//               <HomeItems key={item.url} item={item} id={encodeURIComponent(item.url)} />
-//             ))}
-//           </ul>
-//         </div>
-//         <hr className="line" />
-//         <div className="bottom">
-//           <h1 className="heading">BHARAT NEWS</h1>
-//           <div className="icons">
-//             <CiFacebook className="icon" />
-//             <FiTwitter className="icon" />
-//             <BsInstagram className="icon" />
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default HomeScreen;
